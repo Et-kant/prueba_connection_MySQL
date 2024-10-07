f926c1e1d2aa83f0c38bd3b98c4696eb0a18edc4
Emanuel <gilrestrepoemanuel@gmail.com>
2024-10-07 16:23:41 -0500

    firstCommit

---
+++ DataBase.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE Clientes (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre CHAR(50),
+    Apellido CHAR(50),
+    Email CHAR(100),
+    Telefono CHAR(15)
+);
+	
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = "server= localhost;user id = root; password = 1234; database = new_database_connection";
+
+        using (MySqlConnection conn = new MySqlConnection(connectionString))
+        {
+            try
+            {
+
+                conn.Open();
+                Console.WriteLine("The connection to the data base was successfull");
+            }
+            catch(MySqlException ex)
+            {
+                Console.WriteLine("An error has ocurred" + ex.Message);
+            }
+        }
+    } 
+}
