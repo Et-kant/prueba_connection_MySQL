772714ceffcf3c07e898526e9e43b9bd806c29d7
Emanuel <gilrestrepoemanuel@gmail.com>
2024-10-08 16:03:45 -0500

    addFeatureOfRead

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 ﻿using MySql.Data.MySqlClient;
 using MySqlX.XDevAPI;
 using System;
+using System.Collections.Generic;
 using System.Security.Cryptography;
+using ZstdSharp.Unsafe;
 
-public class Usuario
+public class Usuario //Objeto para crear el usuario
 {
-    public string Nombre { get; set; }
+    public string Nombre { get; set; } //Propiedades
     public string Apellido { get; set; }
     public string Email { get; set; }
     public string Telefono { get; set; }
+    //Constructor 
     public Usuario(String nombre, String apellido, String email, String telefono)
     {
         this.Nombre = nombre;
@@ -63,6 +66,52 @@ public class AccesoaDB
     }
 }
 
+public class ReadDB
+{
+    private String _Stringdeconexion;
+
+    public ReadDB(String conexionString)
+    {
+        _Stringdeconexion = conexionString; 
+    }
+
+    public List<Usuario> GetUsers()
+    {
+        List<Usuario> users = new List<Usuario>();
+        String consulta = "SELECT Nombre, Apellido, Email, Telefono FROM Clientes";
+        using (MySqlConnection con = new MySqlConnection(_Stringdeconexion))
+        {
+            try
+            {
+                con.Open();
+                using (MySqlCommand cmd = new MySqlCommand(consulta, con))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Usuario usuarios = new Usuario(
+                                reader["Nombre"].ToString(),
+                                reader["Apellido"].ToString(),
+                                reader["Email"].ToString(),
+                                reader["Telefono"].ToString()
+                                );
+
+                            users.Add(usuarios);
+                        } 
+                    }
+                }
+            } catch (MySqlException e)
+            {
+                Console.WriteLine("ERROR" + e);
+            }
+        }
+
+        return users;
+    }
+
+}
+
 
 class program
 {
@@ -86,6 +135,15 @@ class program
             Console.WriteLine("El usuario no pudo se añadido");
         }
 
+        ReadDB readDB = new ReadDB(conexionString);
+        List<Usuario> usuarios = readDB.GetUsers();
+
+        Console.WriteLine("\nUsuario en la base de datos");
+
+        foreach(var usr in usuarios)
+        {
+            Console.WriteLine($"Nombre: {usr.Nombre}, Apellido: {usr.Apellido}, Email: {usr.Email}, Telefono: {usr.Telefono}");  
+        }
     }
 
 }
